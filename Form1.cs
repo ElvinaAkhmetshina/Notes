ac57221e422712c5f117001ad4b3d5fccf15ee5e
Elvina Akhmetshina <elv692@gmail.com>
2024-03-12 16:44:56 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApplab3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private BindingList<TableRowData> datalist = new();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = datalist;
+            datalist.Add(new TableRowData(1, '+', 10, "Январь", "Убрать дом", 10, true));
+            datalist.Add(new TableRowData(5, '-', 5, "Октябрь", "Сделать домашнюю работу", 8.4, false));
+            datalist.Add(new TableRowData(2, '+', 6, "Сентябрь", "Купить продукты", 5.3, true));
+            datalist.Add(new TableRowData(10, '+', 8, "Июнь", "Прочитать книгу", 6.1, false));
+        }
+
+        private void addToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var f2 = new EForm();
+            if (f2.ShowDialog(this) == DialogResult.OK)
+            {
+                datalist.Add(f2.UserData);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog(this) == DialogResult.OK)
+            {
+                var filename = saveFileDialog1.FileName;
+                using var file = new FileStream(filename, FileMode.Create);
+                using var sw = new StreamWriter(file, Encoding.UTF8);
+                var jso = new JsonSerializerOptions();
+                jso.WriteIndented = false;
+                foreach (var elem in datalist)
+                {
+                    sw.WriteLine(JsonSerializer.Serialize<TableRowData>(elem, jso));
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog(this) == DialogResult.OK)
+            {
+                var filename = openFileDialog1.FileName;
+                using var sr = new StreamReader(filename, Encoding.UTF8);
+                datalist.Clear();
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine() ?? "";
+                    var obj = JsonSerializer.Deserialize<TableRowData>(line);
+                    if (obj is not null)
+                    {
+                        datalist.Add(obj);
+                    }
+                }
+            }
+        }
+
+        private void editToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int i = dataGridView1.SelectedRows[0].Index;
+                var f2 = new EForm(datalist[i]);
+                if (MessageBox.Show("Вы уверены, что хотите редактировать данное поле?", "Редактировать?",
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    f2.ShowDialog(this);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Выбери строку для редактирования", "Ошибка!");
+            }
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int i = dataGridView1.SelectedRows[0].Index;
+                if (MessageBox.Show("Вы уверены, что хотите удалить данное поле?", "Удалить?",
+                    MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                { 
+                    dataGridView1.Rows.RemoveAt(i); 
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Выбери строку для удаления", "Ошибка!");
+            }
+        }
+    }
+}
---
+++ TableRowData.cs
@@ -0,0 +1,141 @@
+﻿using Hangfire.Annotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApplab3
+{
+    public class TableRowData: INotifyPropertyChanged
+    {
+        private int _index;
+        private int _dayofmonth;
+        private string _month;
+        private string _task;
+        private bool _isdone;
+        private char _type;
+        private double _hard;
+        [DisplayName("Номер задачи")]
+        public int myIndex
+        {
+            get => _index;
+            set
+            {
+                _index = value;
+                OnPropertyChanged();
+            }
+        }
+        [DisplayName("Тип задачи")]
+        public char Type
+        {
+            get => _type;
+            set
+            {
+                _type = value;
+                OnPropertyChanged();
+            }
+        }
+        [DisplayName("День месяца")]
+        public int DayOfMonth
+        {
+            get => _dayofmonth;
+            set
+            {
+                _dayofmonth = value;
+                OnPropertyChanged();
+            }
+        }
+        [DisplayName("Месяц")]
+        public string Month
+        {
+            get => _month;
+            set
+            {
+                _month = value;
+                OnPropertyChanged();
+            }
+        }
+        [DisplayName("Задача")]
+        public string Task
+        {
+            get => _task;
+            set
+            {
+                _task = value;
+                OnPropertyChanged();
+            }
+        }
+        [DisplayName("Сложность задачи")]
+        public double Hard
+        {
+            get => _hard;
+            set
+            {
+                _hard = value;
+                OnPropertyChanged();
+            }
+        }
+        [DisplayName("Задача выполнена?")]
+        public bool isDone
+        {
+            get => _isdone;
+            set
+            {
+                _isdone = value;
+                OnPropertyChanged();
+            }
+        }
+        public TableRowData()
+        {
+            /*myIndex = 1;
+            Type = '+';
+            DayOfMonth = 1;
+            Month = "Январь";
+            Task = "Задача";
+            Hard = 1;
+            isDone = false;*/
+        }
+        public TableRowData(int index, char type, int dayofmonth, string month, string task, double hard, bool isdone)
+        {
+            myIndex = index;
+            Type = type;
+            DayOfMonth = dayofmonth;
+            Month = month;
+            Task = task;
+            Hard = hard;
+            isDone = isdone;
+        }
+        // Метод для копирования объекта
+        public TableRowData Copy() => new TableRowData(myIndex, Type, DayOfMonth, Month, Task, Hard, isDone);
+        // Метод копирования данного объекта в другой, указанный в параметре
+        // (можно использовать для восстановления данных из резервной копии
+        // при отказе пользователя от сохранения сделанных изменений)
+        public void CopyTo(TableRowData? trd)
+        {
+            if (trd is not null)
+            {
+                trd.myIndex = myIndex;
+                trd.Type = Type;
+                trd.DayOfMonth = DayOfMonth;
+                trd.Month = Month;
+                trd.Task = Task;
+                trd.Hard = Hard;
+                trd.isDone = isDone;
+            }
+        }
+
+        // Для автоматического обновления таблицы с данными
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
